e0ff7b13227d2456131fb8a339bced867cee7339
Brent Frakes <brent_frakes@nps.gov>
2016-03-11 15:41:21 -0700

    updated

--- StationList.py
+++ StationList.py
@@ -1,27 +1,23 @@
-#-------------------------------------------------------------------------------
-# Name:       IM_Climate
-# Purpose: Simple API to interface with ACIS Sytsem
-#
-# Author:      Iventory and Montoring Division,  National Park Service
-#               Lisa Nelson, Brent Frakes
-
-#-------------------------------------------------------------------------------
 import urllib2, urllib
 import json
 
 from Station import Station
-import IM_Climate
+from IM_Climate import IM_Climate
 
 class StationList(IM_Climate):
     def __init__(self):
+        super(StationList,self).__init__()
         self.webServiceSource = 'StnMeta'
+        self.stationList = {}
 
     def getStationList(self, state = None, wxElement = None, county = None, bbox = None):
-        return self._call_ACIS(state = state, elems = wxElement
+        results =  self._call_ACIS(state = state, elems = wxElement
             , county = county, bbox = bbox)
+        self.stationList= results['meta']
+        return self.stationList
 
 
 if __name__ == '__main__':
     c = StationList()
-    data =  c.getStationList(state = 'CO', wxElement = 'snwd', county = '08117')
-    print len(data['meta'])
+    stations =  c.getStationList(state = 'CO', wxElement = 'snwd', county = '08117')
+    print len(stations)
