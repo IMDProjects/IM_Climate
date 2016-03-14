3ad1ce13f14f8a908fa1e89a78a2c1e6cda6a854
Brent Frakes <brent_frakes@nps.gov>
2016-03-14 08:51:32 -0600

    ready for demo

--- demo.py
+++ demo.py
@@ -8,11 +8,16 @@ sf = StationFinder()
 stations = sf.find(state = 'CO', wxElement = 'gdd', countyCode = '08117')
 print stations.stationIDs
 print stations.stationNames
+print stations.toJSON()
 
 #Request data for the respective stations
 dr = DataRequestor()
-#All methods of DataRequestor return a WxData dictionary object
+#All methods of DataRequestor return a WxData dictionary object with extended methods
 data = dr.getMonthySummary(stations = stations, wxElement = 'avgt', reduceCode = 'mean', startYear = '1980', endYear = '1981')
+
+#The data object has two components, metadata and data
 print data['meta']
 print data['data']
+
+#One extended method is the JSON export
 print data.toJSON()
