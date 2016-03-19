0972b09aafa4fefcca1c34ed710be4821eb8acae
Brent Frakes <brent_frakes@nps.gov>
2016-03-18 23:01:48 -0600

    updates

--- demo.py
+++ demo.py
@@ -4,11 +4,12 @@ from IM_Climate.DataRequestor import DataRequestor
 
 sf = StationFinder()
 #list county codes to aid in searching
-sf.countyCodes(state = 'CO')
+print(sf.countyCodes(state = 'CO'))
 
 #Find Stations meeting specified criteria
+#Find all stations in Summit County, CO with average temperature
 #The find method returns a stationInfo dictionary object with extended methods
-stationInfo = sf.find(state = 'CO', wxElement = 'avgt', countyCode = '08117')
+stationInfo = sf.find(wxElement = 'avgt', countyCode = '08117')
 print(stationInfo.stationIDs)
 print(stationInfo.stationNames)
 print(stationInfo.metadata)
@@ -18,7 +19,8 @@ print(stationInfo.toJSON())
 #Request data for the respective stations
 #All methods of DataRequestor return a WxData dictionary object with extended methods
 dr = DataRequestor()
-#Get mean monthly values of average temperature from 1970-1975
+#Get mean monthly values of average temperature from 1970-1975. the stationIfno
+#object can be used as the argument for the stations
 wxdata = dr.getMonthySummary(stations = stationInfo, wxElement = 'avgt', reduceCode = 'mean', startYear = '1970', endYear = '1975')
 #Get the daily average temperatures observations, inlcuding flags, for set of stations.
 wxdataData_daily = dr.getDailyWxObservations(stations = stationInfo, wxElement = 'avgt', startDate = '1990-01-01', endDate = '1990-02-05')
