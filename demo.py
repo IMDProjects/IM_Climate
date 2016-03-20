054e14f141401fd9b933c95c538efd804b0c83c1
Brent Frakes <brent_frakes@nps.gov>
2016-03-20 09:21:17 -0600

    cleanup

--- demo.py
+++ demo.py
@@ -8,8 +8,10 @@ print(sf.countyCodes(state = 'CO'))
 
 #Find Stations meeting specified criteria
 #Find all stations in Summit County, CO with average temperature
-#The find method returns a stationInfo dictionary object with extended methods
 stationInfo = sf.find(wxElement = 'avgt', countyCode = '08117')
+
+#The find method returns a stationInfo dictionary object with extended methods
+# and properties
 print(stationInfo.stationIDs)
 print(stationInfo.stationNames)
 print(stationInfo.metadata)
@@ -17,26 +19,27 @@ print(stationInfo.toJSON())
 
 
 #Request data for the respective stations
-#All methods of DataRequestor return a WxData dictionary object with extended methods
 dr = DataRequestor()
 
 #Get mean monthly values of average temperature from 1970-1975. The stationIfno
 #object can be used as the argument for the stations
-wxdata = dr.monthySummary(stations = stationInfo, wxElement = 'avgt', reduceCode = 'mean', startYear = '1970', endYear = '1975')
+wxdata = dr.monthySummary(stations = stationInfo, wxElement = 'avgt',
+    reduceCode = 'mean', startYear = '1970', endYear = '1975')
 
 #Get yearly summary
-wxdata = dr.yearlySummary(stations = stationInfo, wxElement = 'avgt', reduceCode = 'mean', startYear = '1970', endYear = '1975')
+wxdata_yearly = dr.yearlySummary(stations = stationInfo, wxElement = 'avgt',
+    reduceCode = 'mean', startYear = '1970', endYear = '2005')
 
 #Get the daily average temperatures observations, inlcuding flags, for set of stations.
-wxdataData_daily = dr.dailyWxObservations(stations = stationInfo, wxElement = 'avgt', startDate = '1990-01-01', endDate = '1990-02-05')
+wxdataData_daily = dr.dailyWxObservations(stations = stationInfo,
+    wxElement = 'avgt', startDate = '1990-01-01', endDate = '1990-02-05')
 
 
-#All returned objects (stationInfo and WxData) have metadata
+#All methods of DataRequestor return a WxData dictionary object with extended
+# methods and preoperties
 print(wxdata.metadata)  #All metadata
-print(wxdata.dateList)  #The dateList
+print(wxdata.dateList)  #The list of distinct dates
 print(wxdata.getStationData(wxdata.stationIDList[0])) #Get data for first station
-
-#One extended method is the JSON export
-print(wxdata.toJSON())
+print(wxdata.toJSON()) #Export object back to JSON
 
 
