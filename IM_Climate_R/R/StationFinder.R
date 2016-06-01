d7ed48ae071adef19e70c8efbd2cb6e3d0c14dfd
llnelson <lisa_l_nelson@nps.gov>
2016-06-01 10:59:58 -0600

    from Lisa: simple working version of findStation function

--- IM_Climate_R/R/StationFinder.R
+++ IM_Climate_R/R/StationFinder.R
@@ -12,26 +12,30 @@
 
 findStation <- function (parkCodes) {
   # URLs and request parameters
+  
   # NPS Park bounding boxes
   bboxURLBase <- "http://irmaservices.nps.gov/v2/rest/unit/CODE/geography?detail=envelope&dataformat=wkt&format=json"
   bboxExpand  <- 0.33
+  
   # ACIS data services
   baseURL <- "http://data.rcc-acis.org/"
-  webServiceSource <- "StnMeta"
+  webServiceStation <- "StnMeta"
   
-  stationMetadata <-c('uid', 'name', 'state', 'll', 'elev', 'valid_daterange', 'sids')
+  #stationMetadata <-c('uid', 'name', 'state', 'll', 'elev', 'valid_daterange', 'sids')
   parameters <- c('pcpn', 'snwd', 'avgt', 'obst', 'mint', 'snow', 'maxt')
   encode <- c("json")
-  confg <- add_headers(Accept = "'Accept':'application/json'")
+  config <- add_headers(Accept = "'Accept':'application/json'")
   
-  stationURL <- paste(baseURL,webServiceSource)
+  stationURL <- gsub(" ","",paste(baseURL,webServiceStation))
   
+  #Example URLS
   # http://data.rcc-acis.org/StnMeta?bbox=-104.895308730118,%2041.8657116369158,%20-104.197521654032,%2042.5410939149279&meta=uid,%20name,%20state,%20ll,%20elev,%20valid_daterange,sids
+  # http://data.rcc-acis.org/StnMeta?bbox=-104.895308730118,%2041.8657116369158,%20-104.197521654032,%2042.5410939149279
   
   # Get bounding box for park(s)
   bboxURL <- gsub("CODE", parkCodes, bboxURLBase)
   # Counter-clockwise vertices (as WKT): LL, LR, UR, UL
-  bboxWKT <- strsplit(content(GET(bboxURL, confg))[[1]]$Geography, ",")
+  bboxWKT <- strsplit(content(GET(bboxURL, config))[[1]]$Geography, ",")
   # Extract vertices and 'buffer' by 0.3 degrees (~33 km)
   # TODO: add Eastern Hemisphere detection
   LL <- strsplit(substring(bboxWKT[[1]][1], 11), " ")
@@ -50,17 +54,17 @@ findStation <- function (parkCodes) {
   
   bbox  <- paste(c(LLX, LLY, URX, URY), collapse=", ")
   
-  body  <- list(bbox = bbox, meta = stationMetadata)
+  body  <- list(bbox = bbox)
+  #body  <- list(bbox = bbox, meta = stationMetadata)
   #body  <- list(elems = parameters, bbox = bbox, meta = stationMetadata)
-  #bbox  <- c(LLX, LLY, URX, URY)
   
-  #return (bboxWKT[[1]]$Geography)
-  #return (strsplit(bboxWKT[[1]]$Geography, ","))
-  #return (URX)
-  #return (body)
+  # Format GET URL for use in jsonlite request
+  stationRequest <- gsub(" ", "%20", paste(stationURL, body, sep="?bbox="))
   
-  # Use bounding box to request station list
-  stationList  <- POST(stationURL, body = body, encode = "json")
+  # Use bounding box to request station list (jsonlite)
+  stationList <- fromJSON(stationRequest) 
+  # Use bounding box to request station list (httr GET)
+  #stationList  <- content(GET(stationURL, query = body, config = config))
   
-  return (stationList)
+  return (stationList$meta)
 }
