5afe3d5d6870ca27eff839572bfadc7856a3079d
Brent Frakes <brent_frakes@nps.gov>
2016-06-17 14:40:06 -0600

    Ready for testing. Note that I did not include UnitCode in download

--- demo.py
+++ demo.py
@@ -3,13 +3,19 @@ from IM_Climate_py.StationFinder import StationFinder
 localFolder = 'c:\\temp\\'
 sf = StationFinder()
 
-#Case #1: All stations around NOCA
+#Case #1: All stations around NOCA; default to 30km buffer
 data = sf.findStation(unitCode = 'NOCA')
 data.export(localFolder + 'NOCA_Stations.txt')
 
-#Case #2: All stations around NOCA recording minimum temperature
-data = sf.findStation(unitCode = 'NOCA', parameter = 'mint')
-data.export(localFolder + 'minT_Stations.txt')
+#Case #2: All stations around ACAD recording minimum temperature; distance = 10km
+data = sf.findStation(unitCode = 'ACAD', distance = 10, parameter = 'mint')
+data.export(localFolder + 'ACAD_minT_Stations.txt')
+
+#Case 3: All stations around ROMO recording maximum temperature; distance = 40km
+        #with option to save file
+data = sf.findStation(unitCode = 'ROMO', distance = 40, parameter = 'maxt'
+        ,filePathAndName = localFolder + 'ROMO_maxT_Stations.txt')
+
 
 
 
