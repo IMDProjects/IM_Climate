5aa90125f2fc8dd4e6b0929ec8f5cf4e784fff49
llnelson <lisa_l_nelson@nps.gov>
2017-02-06 14:22:15 -0700

    tested re-factoring of getWxObservations; need to re-run roxygenize

--- IM_Climate_R/R/DataRequest.R
+++ IM_Climate_R/R/DataRequest.R
@@ -1,140 +1,5 @@
 #' Get station data for specified parameter(s) and station(s)
 #'
-#' Takes a list of one or more parameters and one or more unique station IDs, requests station data, and returns it as a data frame
-# @param dataURL URL for ACIS data service vending station data
-#' @param climateParameters A list of one or more climate parameters (e.g. pcpn, mint, maxt, avgt, obst, snow, snwd).  If not specified, defaults to all parameters except degree days. See Table 3 on ACIS Web Services page: http://www.rcc-acis.org/docs_webservices.html
-#' @param climateStations A list of one or more unique identifiers (uid) for climate stations. Can be a single item, a list of items, or a data frame of the findStation response.
-#' @param sdate (optional) Default is period of record ("por"). If specific start date is desired, format as a string (yyyy-mm-dd or yyyymmdd). The beginning of the desired date range.
-#' @param edate (optional) Default is period of record ("por"). IF specific end date is desired, format as a string (yyyy-mm-dd or yyyymmdd). The end of the desired date range.
-#' @param filePathAndName (optional) File path and name including extension for output CSV file
-#' @return A data frame containing the requested data. See User Guide for more details: https://docs.google.com/document/d/1B0rf0VTEXQNWGW9fqg2LRr6cHR20VQhFRy7PU_BfOeA/
-#' @examples \dontrun{
-#' Precipitation, temperature weather observations for one station for a specifc date range:
-#'
-#' getDailyWxObservations(climateParameters=list('pcpn', 'avgt', 'obst', 'mint', 'maxt'), climateStations=25056, sdate="20150801", edate="20150831")
-#'
-#' All weather observations for a station for its period of record
-#'
-#' getDailyWxObservations(climateStations=60903)
-#'
-#' All weather observations for all stations (using a findStation response data frame: stationDF) for a specific date range:
-#'
-#' getDailyWxObservations(climateParameters=list('pcpn', 'avgt', 'obst', 'mint', 'maxt'), climateStations=stationDF, sdate="20150801", edate="20150803")
-#' }
-#' @export
-#'
-# TODO: encapsulate in error block
-# See https://github.com/hadley/httr/blob/master/vignettes/api-packages.Rmd
-
-getDailyWxObservations <-
-  function(climateParameters = NULL,
-           climateStations,
-           sdate = "por",
-           edate = "por",
-           filePathAndName = NULL) {
-    # URLs and request parameters:
-    # ACIS data services
-    baseURL <- "http://data.rcc-acis.org/"
-    webServiceSource <- "StnData"
-    duration <- c("dly")
-    # Parameter flags: f = ACIS flag, s = source flag
-    paramFlags <- c("f,s")
-    lookups <- 
-      fromJSON(system.file("ACISLookups.json", package = "IMClimateR"), flatten = TRUE) # assumes placement in package inst subfolder
-    #lookups <- fromJSON("ACISLookups.json", flatten = TRUE)
-    luElements  <- lookups$element
-    
-    # If climateParameters is NULL, default to all parameters except degree days.
-    if (is.null(climateParameters)) {
-      climateParameters0 <- lookups$element$code
-      # Remove degree days (v1.5); super cheesy... fix at some point
-      climateParameters <- climateParameters0[1:7]
-      #climateParameters <- list('pcpn', 'mint', 'maxt', 'avgt', 'obst', 'snow', 'snwd')
-    }
-    
-    # Initialize response object
-    dfResponse <- NULL
-    
-    # Format incoming arguments
-    dataURL <-  gsub(" ", "", paste(baseURL, webServiceSource))
-    
-    # Iterate for each station
-    if (is.data.frame(climateStations)) {
-      listStations = as.list(climateStations$uid)
-    }
-    else if (is.list(climateStations)) {
-      listStations = climateStations
-    }
-    else {
-      listStations = as.list(climateStations)
-    }
-    for (s in 1:length(listStations)) {
-      df <- NULL
-      cUid <- unlist(listStations[s])
-      body <- formatRequest(requestType = "getWxObservations", climateParameters = climateParameters, sdate, edate, cUid, duration = duration, paramFlags = c("f,s"))
-      
-      # This returns the full response - need to use content() and parse
-      # content(dataResponseInit) results in a list lacking column names but containing data which needs to be
-      # converted to dataFrame with appropriate vectors
-      dataResponseInit <-
-        POST(
-          "http://data.rcc-acis.org/StnData",
-          accept_json(),
-          add_headers("Content-Type" = "application/json"),
-          body = body,
-          verbose()
-        )
-      
-      if (grepl("data",content(dataResponseInit, "text")) == FALSE) {
-        dfResponse <- content(dataResponseInit, "text")
-      }
-      else {
-        # Format climate data object
-        rList <- content(dataResponseInit)
-        dataResponseError <- rList$error
-        
-        # Start building the data frame by populating the date vector
-        if (is.null(dataResponseError)) {
-          df <-
-            formatWxObservations(
-              rList,
-              duration = duration,
-              climateParameters = climateParameters,
-              reduceCodes = NULL,
-              luElements = luElements
-            )
-          # Create output object
-          if (is.data.frame(dfResponse)) {
-            dfResponse <- rbind(dfResponse, df)
-          }
-          else {
-            dfResponse <- df
-          }
-        }
-        else {
-          dfResponse <- dataResponseError
-        }
-      }
-    }
-    # Output file
-    if (!is.null(filePathAndName)) {
-      write.table(
-        dfResponse,
-        file = filePathAndName,
-        sep = ",",
-        row.names = FALSE,
-        qmethod = "double"
-      )
-    }
-    else {
-      return (dfResponse)
-    }
-    return (dfResponse)
-  }
-
-
-#' Get station data for specified parameter(s) and station(s)
-#'
 #' Takes a list of one or more parameters and one or more unique station IDs, requests station data, and returns it as a data frame. Note: For monthly data, value vectors returned as character format to accommodate missing records ("M")
 # @param dataURL URL for ACIS data service vending station data
 #' @param climateStations A list of one or more unique identifiers (uid) for climate stations. Can be a single item, a list of items, or a data frame of the findStation response.
@@ -153,7 +18,7 @@ getDailyWxObservations <-
 #'
 #' The same request written to a CSV file:
 #' 
-#' getWxObservations(climateParameters=list('pcpn', 'avgt', 'obst', 'mint', 'maxt'), climateStations=25056, sdate="20150801", edate="20160831", filePathAndName = "D:\\temp\\trash\\monthyObs_station25056.csv")
+#' getWxObservations(climateParameters=list('pcpn', 'avgt', 'obst', 'mint', 'maxt'), climateStations=25056, sdate="20150801", edate="20160831", filePathAndName = "D:\\temp\\trash\\dailyObs_station25056.csv")
 #'  
 #' All daily weather observations for a station for its period of record
 #'
@@ -163,7 +28,7 @@ getDailyWxObservations <-
 #' 
 #' getWxObservations(climateStations = list(61193, 26215), sdate="201401", edate = "201501", duration="mly", maxMissing = NULL)
 #'
-#' Monthly weather observations for minimum precipitation for a station from beginning of record through Sept 2016
+#' Monthly weather observations for precipitation for a station from beginning of record through Sept 2016
 #' 
 #' getWxObservations(climateStations = list(26215), climateParameters = list('pcpn'), reduceCodes = list('min'), edate= "2016-09", duration="mly", maxMissing = 2)
 #'
