2eddb7d0eef39d86986ea7d45da0ba627540995a
llnelson <lisa_l_nelson@nps.gov>
2017-09-12 09:29:46 -0600

    Added explicit parameter and param dates to output.  Loops through param list.

--- IM_Climate_R/R/StationFinder.R
+++ IM_Climate_R/R/StationFinder.R
@@ -80,86 +80,99 @@ findStation <- function (unitCode, distance=NULL, climateParameters=NULL, filePa
     climateParameters <- climateParameters0[1:7]
     #climateParameters = parameters
   }
-  stationRequest <- gsub(" ", "%20", paste(paste(paste(stationURL, paste(climateParameters, collapse = ","), sep="?elems="), body, sep="&bbox="), paste(stationMetadata, collapse=","), sep="&meta="))
-  #stationRequest <- gsub(" ", "%20", paste(paste(stationURL, paste(climateParameters, collapse = ","), sep="?elems="), body, sep="&bbox="))
-  #stationRequest <- gsub(" ", "%20", paste(paste(paste(stationURL, paste(climateParameters, collapse = ","), sep="?elems="), body, sep="&bbox=")),paste(stationMetadata, collapse = ","), sep="&meta=")
-  
-  # Use bounding box to request station list (jsonlite)
-  stationListInit <- fromJSON(stationRequest) 
-  if (length(stationListInit$meta) > 0) {
-    uid <- setNames(as.data.frame(as.numeric(stationListInit$meta$uid)), "uid")
-    longitude <- setNames(as.data.frame(as.numeric(as.matrix(lapply(stationListInit$meta$ll, function(x) unlist(as.numeric(x[1])))))),"longitude")
-    latitude <- setNames(as.data.frame(as.numeric(as.matrix(lapply(stationListInit$meta$ll, function(x) unlist(as.numeric(x[2])))))),"latitude")
-    # Check for presence of all SID values (use max of 3 per record even if station has > 3)
-    # Suppress warnings from getStationSubtype(): raised due to conversion necessary because data.frame vector access does not recognize column name
-    sid1 = c()
-    sid2 = c()
-    sid3 = c()
-    sid1_type = c(as.character(NA))
-    sid2_type = c(as.character(NA))
-    sid3_type = c(as.character(NA))
-    minDate = c(as.Date(NA))
-    maxDate = c(as.Date(NA))
-    for (i in 1:length(stationListInit$meta$sids)) {
-      if (length(unlist(stationListInit$meta$sids[i])) >= 3) {
-        sid1[i] <- as.character(as.vector(lapply(stationListInit$meta$sids[i], function(x) unlist(x[1]))))
-        sid1_type[i] <-  suppressWarnings(getStationSubtype(unlist(strsplit(sid1[i], " "))[2], substr(sid1[i],1,3)))
-        
-        sid2[i] <- as.character(as.vector(lapply(stationListInit$meta$sids[i], function(x) unlist(x[2]))))
-        sid2_type[i] <-  suppressWarnings(getStationSubtype(unlist(strsplit(sid2[i], " "))[2], substr(sid2[i],1,3)))
-        sid3[i] <- as.character(as.vector(lapply(stationListInit$meta$sids[i], function(x) unlist(x[3]))))
-        sid3_type[i] <-  suppressWarnings(getStationSubtype(unlist(strsplit(sid3[i], " "))[2], substr(sid3[i],1,3)))
+  stationList = NULL
+  for (p in 1:length(climateParameters)) {
+    stationRequest <- gsub(" ", "%20", paste(paste(paste(stationURL, paste(climateParameters[p], collapse = ","), sep="?elems="), body, sep="&bbox="), paste(stationMetadata, collapse=","), sep="&meta="))
+    #stationRequest <- gsub(" ", "%20", paste(paste(paste(stationURL, paste(climateParameters, collapse = ","), sep="?elems="), body, sep="&bbox="), paste(stationMetadata, collapse=","), sep="&meta="))
+    #stationRequest <- gsub(" ", "%20", paste(paste(stationURL, paste(climateParameters, collapse = ","), sep="?elems="), body, sep="&bbox="))
+    #stationRequest <- gsub(" ", "%20", paste(paste(paste(stationURL, paste(climateParameters, collapse = ","), sep="?elems="), body, sep="&bbox=")),paste(stationMetadata, collapse = ","), sep="&meta=")
+    
+    # Use bounding box to request station list (jsonlite)
+    stationListInit <- fromJSON(stationRequest) 
+    if (length(stationListInit$meta) > 0) {
+      uid <- setNames(as.data.frame(as.numeric(stationListInit$meta$uid)), "uid")
+      longitude <- setNames(as.data.frame(as.numeric(as.matrix(lapply(stationListInit$meta$ll, function(x) unlist(as.numeric(x[1])))))),"longitude")
+      latitude <- setNames(as.data.frame(as.numeric(as.matrix(lapply(stationListInit$meta$ll, function(x) unlist(as.numeric(x[2])))))),"latitude")
+      # Check for presence of all SID values (use max of 3 per record even if station has > 3)
+      # Suppress warnings from getStationSubtype(): raised due to conversion necessary because data.frame vector access does not recognize column name
+      sid1 = c()
+      sid2 = c()
+      sid3 = c()
+      sid1_type = c(as.character(NA))
+      sid2_type = c(as.character(NA))
+      sid3_type = c(as.character(NA))
+      minDate = c(as.Date(NA))
+      maxDate = c(as.Date(NA))
+      for (i in 1:length(stationListInit$meta$sids)) {
+        if (length(unlist(stationListInit$meta$sids[i])) >= 3) {
+          sid1[i] <- as.character(as.vector(lapply(stationListInit$meta$sids[i], function(x) unlist(x[1]))))
+          sid1_type[i] <-  suppressWarnings(getStationSubtype(unlist(strsplit(sid1[i], " "))[2], substr(sid1[i],1,3)))
+          
+          sid2[i] <- as.character(as.vector(lapply(stationListInit$meta$sids[i], function(x) unlist(x[2]))))
+          sid2_type[i] <-  suppressWarnings(getStationSubtype(unlist(strsplit(sid2[i], " "))[2], substr(sid2[i],1,3)))
+          sid3[i] <- as.character(as.vector(lapply(stationListInit$meta$sids[i], function(x) unlist(x[3]))))
+          sid3_type[i] <-  suppressWarnings(getStationSubtype(unlist(strsplit(sid3[i], " "))[2], substr(sid3[i],1,3)))
+        }
+        else if (identical(length(unlist(stationListInit$meta$sids[i])), as.integer(c(2)))) {
+          sid1[i] <- as.character(as.vector(lapply(stationListInit$meta$sids[i], function(x) unlist(x[1]))))
+          sid1_type[i] <-  suppressWarnings(getStationSubtype(unlist(strsplit(sid1[i], " "))[2], substr(sid1[i],1,3)))
+          sid2[i] <- as.character(as.vector(lapply(stationListInit$meta$sids[i], function(x) unlist(x[2]))))
+          sid2_type[i] <-  suppressWarnings(getStationSubtype(unlist(strsplit(sid2[i], " "))[2], substr(sid2[i],1,3)))
+          sid3[i] <- as.character(NA)
+          sid3_type[i] <-  as.character(NA)
+        }
+        else {
+          sid1[i] <- as.character(as.vector(lapply(stationListInit$meta$sids[i], function(x) unlist(x[1]))))
+          sid1_type[i] <-  suppressWarnings(getStationSubtype(unlist(strsplit(sid1[i], " "))[2], substr(sid1[i],1,3)))
+          sid2[i] <- as.character(NA)
+          sid2_type[i] <-  as.character(NA)
+          sid3[i] <- as.character(NA)
+          sid3_type[i] <-  as.character(NA)
+        }
       }
-      else if (identical(length(unlist(stationListInit$meta$sids[i])), as.integer(c(2)))) {
-        sid1[i] <- as.character(as.vector(lapply(stationListInit$meta$sids[i], function(x) unlist(x[1]))))
-        sid1_type[i] <-  suppressWarnings(getStationSubtype(unlist(strsplit(sid1[i], " "))[2], substr(sid1[i],1,3)))
-        sid2[i] <- as.character(as.vector(lapply(stationListInit$meta$sids[i], function(x) unlist(x[2]))))
-        sid2_type[i] <-  suppressWarnings(getStationSubtype(unlist(strsplit(sid2[i], " "))[2], substr(sid2[i],1,3)))
-        sid3[i] <- as.character(NA)
-        sid3_type[i] <-  as.character(NA)
+      #sid1 <- setNames(sid1,"sid1")
+      sid1 <- setNames(as.data.frame(sid1),"sid1")
+      sid2 <- setNames(as.data.frame(sid2),"sid2")
+      sid3 <- setNames(as.data.frame(sid3),"sid3")
+      sid1_type <- setNames(as.data.frame(sid1_type),"sid1_type")
+      sid2_type <- setNames(as.data.frame(sid2_type),"sid2_type")
+      sid3_type <- setNames(as.data.frame(sid3_type),"sid3_type")
+      i <- NULL
+      for (i in 1:length(stationListInit$meta$sids)) {
+        minDate[i] <- as.Date(range(unlist(stationListInit$meta$valid_daterange[i]))[1], "%Y-%m-%d")
+        maxDate[i] <- as.Date(range(unlist(stationListInit$meta$valid_daterange[i]))[2], "%Y-%m-%d")
+      }
+      minDate <-  setNames(as.data.frame(minDate), "minDate")
+      maxDate <-  setNames(as.data.frame(maxDate), "maxDate")
+      # Force elevation to be numeric with precision of 1
+      options(digits = 1)
+      elev <- as.numeric(stationListInit$meta[,5])
+      options(digits = 7)
+      if (is.null(stationList)) {
+        stationList <- cbind( uid, name=stationListInit$meta[,1], longitude, latitude, sid1, sid1_type, sid2, sid2_type, sid3, sid3_type, state=stationListInit$meta[,4], elev=stationListInit$meta[,5], minDate, maxDate)
+        stationList$climateParameter <- unlist(climateParameters[p])
+        stationList$unitCode <- unitCode[1]
       }
       else {
-        sid1[i] <- as.character(as.vector(lapply(stationListInit$meta$sids[i], function(x) unlist(x[1]))))
-        sid1_type[i] <-  suppressWarnings(getStationSubtype(unlist(strsplit(sid1[i], " "))[2], substr(sid1[i],1,3)))
-        sid2[i] <- as.character(NA)
-        sid2_type[i] <-  as.character(NA)
-        sid3[i] <- as.character(NA)
-        sid3_type[i] <-  as.character(NA)
+        stationListTemp <- cbind( uid, name=stationListInit$meta[,1], longitude, latitude, sid1, sid1_type, sid2, sid2_type, sid3, sid3_type, state=stationListInit$meta[,4], elev=stationListInit$meta[,5], minDate, maxDate)
+        stationListTemp$climateParameter <- unlist(climateParameters[p])
+        stationListTemp$unitCode <- unitCode[1]
+        stationList <- rbind(stationList, stationListTemp)
       }
+      # Convert factors to character vectors
+      fc  <- sapply(stationList, is.factor)
+      lc <- sapply(stationList, is.logical)
+      stationList[, fc]  <- sapply(stationList[, fc], as.character)
+      stationList[, lc]  <- sapply(stationList[, lc], as.character)
     }
-    #sid1 <- setNames(sid1,"sid1")
-    sid1 <- setNames(as.data.frame(sid1),"sid1")
-    sid2 <- setNames(as.data.frame(sid2),"sid2")
-    sid3 <- setNames(as.data.frame(sid3),"sid3")
-    sid1_type <- setNames(as.data.frame(sid1_type),"sid1_type")
-    sid2_type <- setNames(as.data.frame(sid2_type),"sid2_type")
-    sid3_type <- setNames(as.data.frame(sid3_type),"sid3_type")
-    i <- NULL
-    for (i in 1:length(stationListInit$meta$sids)) {
-      minDate[i] <- as.Date(range(unlist(stationListInit$meta$valid_daterange[i]))[1], "%Y-%m-%d")
-      maxDate[i] <- as.Date(range(unlist(stationListInit$meta$valid_daterange[i]))[2], "%Y-%m-%d")
+    else {
+      stationList <- NA # per John Paul's request - Issue #49
+      #stationList <- cat("No stations for ", unitCode, "using distance ", distance) 
     }
-    minDate <-  setNames(as.data.frame(minDate), "minDate")
-    maxDate <-  setNames(as.data.frame(maxDate), "maxDate")
-    # Force elevation to be numeric with precision of 1
-    options(digits = 1)
-    elev <- as.numeric(stationListInit$meta[,5])
-    options(digits = 7)
-    stationList <- cbind( uid, name=stationListInit$meta[,1], longitude, latitude, sid1, sid1_type, sid2, sid2_type, sid3, sid3_type, state=stationListInit$meta[,4], elev=stationListInit$meta[,5], minDate, maxDate)
-    stationList$unitCode <- unitCode[1]
-    # Convert factors to character vectors
-    fc  <- sapply(stationList, is.factor)
-    lc <- sapply(stationList, is.logical)
-    stationList[, fc]  <- sapply(stationList[, fc], as.character)
-    stationList[, lc]  <- sapply(stationList[, lc], as.character)
-  }
-  else {
-    stationList <- NA # per John Paul's request - Issue #49
-    #stationList <- cat("No stations for ", unitCode, "using distance ", distance) 
   }
   # Output file
   if (!is.null(filePathAndName)) {
-    write.table(stationList, file=filePathAndName, sep=",", row.names=FALSE, qmethod="double")
+    write.table(stationList, file=filePathAndName, append = TRUE, sep=",", row.names=FALSE, qmethod="double")
   }
   else {
     return (stationList)
